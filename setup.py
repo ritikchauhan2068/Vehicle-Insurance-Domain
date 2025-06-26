28f3df564eb863a4234c7bad13a00c3f72c8f24d
ritik <ritikchauhan2068@gmail.com>
2025-06-26 14:57:41 +0530

    project struture ans setup ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ritik Chauhan",
+    author_email="ritikchauhan2068@gmail.com",
+    packages=find_packages()
+)
