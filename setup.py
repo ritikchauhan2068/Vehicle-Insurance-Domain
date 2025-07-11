07f6cd10a5f9ac73cce828ac9396ececf6cf90ec
ritik <ritikchauhan2068@gmail.com>
2025-07-11 17:08:49 +0530

    Complete Project

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Ritik Chauhan",
-    author_email="ritikchauhan2068@gmail.com",
+    author="Vikash Das",
+    author_email="vikashdas770@gmail.com",
     packages=find_packages()
 )
